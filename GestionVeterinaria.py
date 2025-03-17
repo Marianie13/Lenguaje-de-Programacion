a61748fca0e460e69b4d0863ae7144c28d793cbe
Marianie Vásquez <99088963+Marianie13@users.noreply.github.com>
2025-03-17 16:39:42 -0500

    Se agrega la funsión para agregar una mascota

--- GestionVeterinaria.py
+++ GestionVeterinaria.py
@@ -1,6 +1,7 @@
+datos_mascotas ={}
 
 while True:
-    print("\n---MENU GESTION DE VERINARIA---")
+    print("\n===MENU GESTION DE VERINARIA===")
     print("1. Registrar Mascota")
     print("2. Agedar una cita veterinaria")
     print("3. Calcular el costo del servicio")
@@ -11,3 +12,14 @@ while True:
     except ValueError:
         print("Error ingrese numero Válido. ")
         continue
+    
+    def registar_mascota():
+        nombre = input("Ingrese el nombre de la mascota: ")
+        especie = input("Ingrese la especie(gato, perro, conejo, etc.): ")
+        edad = input("Ingrese la edad de la mascota: ")
+        dueno = input("Ingrese el nombre del dueño: ")
+        datos_mascotas[nombre] = {"especie": especie, "edad": edad, "dueño": dueno}
+        print("Mascota registarda exitosamente")
+        
+        
+        
