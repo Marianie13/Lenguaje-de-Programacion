d1365605c1460e237c0122eca537d3ba38869704
Marianie Vásquez <99088963+Marianie13@users.noreply.github.com>
2025-03-23 13:45:01 -0500

    Se hace docuemnto de gestion de Jardin Botanico

---
+++ Primer Corte/GestionJardinBotanico.py
@@ -0,0 +1,13 @@
+while True:
+    print("=== GESTION JARDIN BOTANICO ===")
+    print("1. Registar una planta")
+    print("2. Consultar información de una planta")
+    print("3. Registar un visitante")
+    print("4. Rejistar una venta")
+    print("5. Calcular mantenimiento")
+    print("6. Calcular mantenimiento")
+    print("7. Salir del programa")
+    
+
+    
+    
